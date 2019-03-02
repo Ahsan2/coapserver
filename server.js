d5c76a68f631d9caf05160b7a2f6be3b49d2addc
Ahsan Masood <ahsan21916@gmail.com>
2019-03-02 22:54:05 +0500

    first commit

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 var coap        = require('coap')
   , server      = coap.createServer()
  
-const PORT = process.env.PORT || 5683;
-
 server.on('request', function(req, res) {
   res.end('Hello ' + req.url.split('/')[1] + '\n')
 })
  
 // the default CoAP port is 5683
-server.listen(PORT, function() {
+server.listen(function() {
 //   var req = coap.request('coap://localhost/Matteo')
  
 //   req.on('response', function(res) {
