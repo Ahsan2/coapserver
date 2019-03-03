97f02d31a0e5312a9fd5939a31bb3b09ff6a2f39
Ahsan Masood <ahsan21916@gmail.com>
2019-03-03 18:31:55 +0500

    misc

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ var coap        = require('coap')
   , server      = coap.createServer()
  
 server.on('request', function(req, res) {
-  res.end('Hello ')
+  res.end('Hello ' + req.url)
+  console.log(req.payload.toString('utf8'))
   // res.end('Hello ' + req.url.split('/')[1] + '\n')
 })
  
